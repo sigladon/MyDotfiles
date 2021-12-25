a96bae7eda1d1fabeecfdaf304b304c6f46668cc
sigladon <sigladon.rc@gmail.com>
2021-12-25 18:29:43 -0500

    primer commit :

---
+++ nvim/.config/nvim/lua/settings.lua
@@ -0,0 +1,22 @@
+local o = vim.o
+local wo = vim.wo
+
+o.autoindent = true
+o.copyindent = true
+o.background = "dark"
+o.backup = false
+o.clipboard = "unnamedplus"
+o.cmdheight = 4
+--o.cursorline = true
+--o.cursorlineopt = "number"
+o.encoding = "UTF-8"
+o.expandtab = true
+o.hidden = true
+o.laststatus = 0
+o.mouse = "a"
+o.showmode = false
+o.wrap = true
+o.ruler = true
+o.tabstop = 2
+o.number = true
+o.relativenumber = true

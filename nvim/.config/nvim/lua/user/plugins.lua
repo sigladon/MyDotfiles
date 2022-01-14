0858dee539ff378a188345ffe01eefb904106afb
Rafael Baculima <sigladon.rc@gmail.com>
2022-01-14 17:30:13 -0500

    Añadidas varias cosas

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -46,7 +46,7 @@ return packer.startup(function(use)
   use "tpope/vim-endwise"
   use "tpope/vim-surround"
   use "9mm/vim-closer" 
- -- use "phanviet/vim-monokai-pro"
+  -- ColorScheme
   use "tanvirtin/monokai.nvim"
 
   -- cmp plugins
@@ -55,9 +55,17 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-path" -- path completions
   use "hrsh7th/cmp-cmdline" -- cmdline completions
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/cmp-nvim-lua"
+
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use use "saadparwaiz1/cmp_luasnip" -- snippet completions
+
+  -- LSP
+  use "neovim/nvim-lspconfig"
+  use "williamboman/nvim-lsp-installer"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
